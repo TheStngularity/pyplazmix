67639144be8c72ed62acae74833cee8196857ca0
TheStngularity <92825138+TheStngularity@users.noreply.github.com>
2021-11-16 17:18:23 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
 	name="pyplazmix",
 	author="The Singularity",
 	url="https://github.com/TheStngularity/pyplazmix",
-	version="0.0.1",
+	version="0.0.2",
 	description="A python wrapper for PlazmixAPI",
 	long_description=readme,
 	long_description_content_type="text/markdown",
